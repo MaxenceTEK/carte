716f655c43424e8d3d812a09025dccbb55f0d1c9
maxence <maxence.herault@gmail.com>
2021-02-18 13:40:34 +0100

    lacarte

--- app/src/main/java/com/example/lacarte/MapsActivity.java
+++ app/src/main/java/com/example/lacarte/MapsActivity.java
@@ -2,6 +2,8 @@ package com.example.lacarte;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
@@ -11,15 +13,20 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.internal.IMapFragmentDelegate;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
@@ -31,12 +38,16 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import static com.example.lacarte.R.id.drawerlayout2;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     List<Address> monadresse;
     List<Address> ladresse;
     private GoogleMap mMap;
     FusedLocationProviderClient fusedLocationProviderClient;
-
+    SearchView searchView;
+    SupportMapFragment mapFragment;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,26 +55,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        searchView = findViewById(R.id.recherche);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String rechadresse = searchView.getQuery().toString();
+                List<Address> addressList = null;
+                if (rechadresse != null || !rechadresse.equals("")) {
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(rechadresse, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(rechadresse));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+        });
         mapFragment.getMapAsync(this);
     }
-
-    public FusedLocationProviderClient getFusedLocationProviderClient() {
+    public FusedLocationProviderClient getFusedLocationProviderClient () {
         return fusedLocationProviderClient;
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
         if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             markyourloc();
         } else {
@@ -73,7 +107,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
-        // Add a marker in Sydney and move the camera
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
@@ -157,4 +190,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
+    private void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer((GravityCompat.START));
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public void ClickHome(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public void Clickmenu(View view) {
+        openDrawer(drawerLayout);
+    }
 }
